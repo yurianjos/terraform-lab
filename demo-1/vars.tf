28c034870be99276ba45349c8acd37f7e5ac2114
Yuri dos Anjos <anjos.yuri@gmail.com>
2020-03-02 15:57:06 -0300

    Teste

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,11 +1,11 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-east-1"
+  default = "eu-west-1"
 }
 
 variable "AMIS" {
-  type = map(string)
+  type = "${map(string)}"
   default = {
     us-east-1 = "ami-0a887e401f7654935"
     us-west-2 = "ami-0313f6bdc53e1536d"
